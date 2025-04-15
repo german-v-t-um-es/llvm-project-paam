828a25f8fe3224abc65bd5973741d46d9bff11fc
german-v-t-um-es <german.v.t@um.es>
2025-04-15 11:44:24 +0200

    Ampliamos el pass para incluir memoria

--- llvm/lib/Transforms/Utils/SampleCudaPass.cpp
+++ llvm/lib/Transforms/Utils/SampleCudaPass.cpp
@@ -5,13 +5,30 @@
 #include "llvm/IR/CallingConv.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Format.h"
 
 using namespace llvm;
+
+bool isArithmetic(unsigned opcode) {
+  return opcode == Instruction::Add  ||
+         opcode == Instruction::FAdd ||
+         opcode == Instruction::Sub  ||
+         opcode == Instruction::FSub ||
+         opcode == Instruction::Mul  ||
+         opcode == Instruction::FMul ||
+         opcode == Instruction::UDiv ||
+         opcode == Instruction::SDiv ||
+         opcode == Instruction::FDiv ||
+         opcode == Instruction::URem ||
+         opcode == Instruction::SRem ||
+         opcode == Instruction::FRem;
+}
 
 PreservedAnalyses SampleCudaPass::run(Function &F,
                     FunctionAnalysisManager &AM) {
   errs() << "Analyzing: " << F.getName() << "\n";
 
+  // Begin identifying GPU kernel
   bool isGPUKernel = false;
 
   // Check for common GPU kernel calling conventions
@@ -29,17 +46,43 @@ PreservedAnalyses SampleCudaPass::run(Function &F,
   } else {
   errs() << "Not a GPU kernel: " << F.getName() << "\n";
   }
+  // End identifying GPU kernel
 
-  uint64_t count = 0;
+  // Begin calculating arithmetic intensity / memory operations
+  double insts = 0.0;
+  double arithmetic_insts = 0.0;
+
+  double memory_ops = 0.0;
+  double memory_loads = 0.0;
+  double memory_stores = 0.0;
+
   for(auto &BB : F) {
     for(auto &I : BB) {
+      insts++;
       // errs() << I << "\n";
-      if (I.getOpcode() == Instruction::Load) {
-        count++;
+      unsigned opcode = I.getOpcode();
+      if (isArithmetic(opcode)) {
+        arithmetic_insts++; // arithmetic operations
+      } else if (opcode == Instruction::Load || opcode == Instruction::Store) {
+        memory_ops++; // memory operations
+        if (opcode == Instruction::Load) {
+          memory_loads++; // loads
+        } else if (opcode == Instruction::Store) {
+          memory_stores++; // stores
+        }
       }
     }
   }
-  errs() << "Number of loads: " << count << "\n";
+
+  double ai = (arithmetic_insts/insts);
+  errs() << "All instructions: " << format("%.0f", insts) << "\n";
+  errs() << "Arithmetic instructions: " << format("%.0f", arithmetic_insts) << "\n";
+  errs() << "Arithmetic intensity: " << format("%.5f", ai) << "\n";
+  errs() << "Memory operations: " << format("%.0f", memory_ops) << "\n";
+  errs() << "Memory loads: " << format("%.0f", memory_loads) << "\n";
+  errs() << "Memory stores: " << format("%.0f", memory_stores) << "\n";
+  errs() << "\n";
+  // End calculating arithmetic intensity / memory operations
 
   return PreservedAnalyses::all();
 }
